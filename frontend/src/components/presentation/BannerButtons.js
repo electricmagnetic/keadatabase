669ce0a9ed299a5d71837f265c0cfc96ae26a7cb
George Moon <gmoon@doc.govt.nz>
2021-09-21 10:29:52 +1200

    Added Sponsor a Kea button

--- frontend/src/components/presentation/BannerButtons.js
+++ frontend/src/components/presentation/BannerButtons.js
@@ -13,25 +13,32 @@ const BannerButtons = ({ className }) => {
   return (
     <div className={classNames.join(' ')}>
       <div className="container">
-        <div className="row">
-          <div className="col-md-4 banner-button">
+        <div className="form-row my-n2">
+          <div className="col-sm-6 col-md-3 my-2 banner-button">
             <Link to="/report" className="btn btn-lg btn-transparent">
-              <i className="fa-fw fas fa-eye mr-2" />
+              <i className="fa-fw fas fa-eye mr-1" />
               Report
               <span className="d-md-none d-lg-inline"> Observation</span>
             </Link>
           </div>
-          <div className="col-md-4 banner-button">
+          <div className="col-sm-6 col-md-3 my-2 banner-button">
             <Link to="/birds" className="btn btn-lg btn-transparent">
-              <i className="fa-fw fas fa-search mr-2" />
+              <i className="fa-fw fas fa-search mr-1" />
               <span className="d-md-none d-lg-inline">Search </span>Birds
             </Link>
           </div>
-          <div className="col-md-4 banner-button">
+          <div className="col-sm-6 col-md-3 my-2 banner-button">
             <Link to="/observations" className="btn btn-lg btn-transparent">
-              <i className="fa-fw fas fa-list-alt mr-2" />
+              <i className="fa-fw fas fa-list-alt mr-1" />
               <span className="d-md-none d-lg-inline">View </span>
               Observations
+            </Link>
+          </div>
+          <div className="col-sm-6 col-md-3 my-2 banner-button">
+            <Link to="/sponsor" className="btn btn-lg btn-transparent">
+              <i className="fa-fw fas fa-hands-helping mr-1" />
+              Sponsor
+              <span className="d-md-none d-lg-inline"> a Kea</span>
             </Link>
           </div>
         </div>

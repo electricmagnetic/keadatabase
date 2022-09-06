22b673f3105ab9d94c347e34e9b8e7ca5778ac01
George Moon <george@moon.org.nz>
2022-09-06 21:52:40 +0200

    Full geospatial search

--- backend/src/sightings/views/observations.py
+++ backend/src/sightings/views/observations.py
@@ -1,9 +1,26 @@
 from rest_framework import viewsets
+#from rest_framework_gis.filters import InBBoxFilter
+from rest_framework_gis.filterset import GeoFilterSet
+from rest_framework_gis.filters import GeometryFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import FilterSet
 
 from keadatabase.pagination import ObservationPagination
 from ..models.observations import Sighting
 from ..serializers.observations import ObservationSerializer
 
+class ObservationFilter(GeoFilterSet):
+    point_location = GeometryFilter(lookup_expr='within')
+
+    class Meta:
+        model = Sighting
+        fields = (
+            'sighting_type',
+            'precision',
+            'number',
+            'status',
+            'confirmed',
+        )
 
 class ObservationViewSet(viewsets.ModelViewSet):
     queryset = Sighting.objects. \
@@ -11,13 +28,10 @@ class ObservationViewSet(viewsets.ModelViewSet):
                exclude(status='private').exclude(status='bad')
     serializer_class = ObservationSerializer
     pagination_class = ObservationPagination
-    filter_fields = (
-        'sighting_type',
-        'precision',
-        'number',
-        'status',
-        'confirmed',
-    )
+    filterset_class = ObservationFilter
+    #filter_backends = (InBBoxFilter, DjangoFilterBackend)
+    #bbox_filter_field = 'point_location'
+    #bbox_filter_include_overlapping = True # Optional
     ordering_fields = (
         'contributor',
         'region',

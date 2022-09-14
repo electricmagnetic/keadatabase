61721b9144793be0276b38bd0cea8bf638fad7b8
George Moon <george@moon.org.nz>
2022-09-14 09:37:28 +0200

    Revert "Added geospatial filtering to observations (#39)"
    This reverts commit 816340779b5b2a2771bd0cb291978cf4b8454d3b.

--- backend/src/sightings/views/observations.py
+++ backend/src/sightings/views/observations.py
@@ -1,23 +1,9 @@
 from rest_framework import viewsets
-from rest_framework_gis.filters import GeometryFilter
-from django_filters import FilterSet
 
 from keadatabase.pagination import ObservationPagination
 from ..models.observations import Sighting
 from ..serializers.observations import ObservationSerializer
 
-class ObservationFilter(FilterSet):
-    point_location = GeometryFilter(lookup_expr='within')
-
-    class Meta:
-        model = Sighting
-        fields = (
-            'sighting_type',
-            'precision',
-            'number',
-            'status',
-            'confirmed',
-        )
 
 class ObservationViewSet(viewsets.ModelViewSet):
     queryset = Sighting.objects. \
@@ -25,7 +11,13 @@ class ObservationViewSet(viewsets.ModelViewSet):
                exclude(status='private').exclude(status='bad')
     serializer_class = ObservationSerializer
     pagination_class = ObservationPagination
-    filterset_class = ObservationFilter
+    filter_fields = (
+        'sighting_type',
+        'precision',
+        'number',
+        'status',
+        'confirmed',
+    )
     ordering_fields = (
         'contributor',
         'region',

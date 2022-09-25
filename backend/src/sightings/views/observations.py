3cc3ca3e98a6b36db4af77712ebfd036e81669f0
George Moon <george@moon.org.nz>
2022-09-25 14:29:03 +0200

    Adding geospatial filtering back

--- backend/src/sightings/views/observations.py
+++ backend/src/sightings/views/observations.py
@@ -1,9 +1,23 @@
 from rest_framework import viewsets
+from rest_framework_gis.filters import GeometryFilter
+from django_filters import FilterSet
 
 from keadatabase.pagination import ObservationPagination
 from ..models.observations import Sighting
 from ..serializers.observations import ObservationSerializer
 
+class ObservationFilter(FilterSet):
+    point_location = GeometryFilter(lookup_expr='within')
+
+    class Meta:
+        model = Sighting
+        fields = (
+            'sighting_type',
+            'precision',
+            'number',
+            'status',
+            'confirmed',
+        )
 
 class ObservationViewSet(viewsets.ModelViewSet):
     queryset = Sighting.objects. \
@@ -11,13 +25,7 @@ class ObservationViewSet(viewsets.ModelViewSet):
                exclude(status='private').exclude(status='bad')
     serializer_class = ObservationSerializer
     pagination_class = ObservationPagination
-    filter_fields = (
-        'sighting_type',
-        'precision',
-        'number',
-        'status',
-        'confirmed',
-    )
+    filterset_class = ObservationFilter
     ordering_fields = (
         'contributor',
         'region',

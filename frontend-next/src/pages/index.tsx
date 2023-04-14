d67323dcdf9680dbb88f56457a112d345e2cd4c9
George Moon <george_moon@natural-solutions.eu>
2023-04-14 20:58:43 +0200

    Added Next empty framework

---
+++ frontend-next/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Kea Database</title>
+      </Head>
+      <main>
+        <h1>Kea Database v3</h1>
+      </main>
+    </>
+  )
+}

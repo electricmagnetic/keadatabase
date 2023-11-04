0d367f339936989c957a93bd6309629a1790d6e9
George Moon <george@carbonfarm.tech>
2023-11-04 13:48:20 +0100

    Initial next commit

---
+++ frontend-next/.eslintrc.js
@@ -0,0 +1,33 @@
+const { resolve } = require("node:path");
+
+const project = resolve(__dirname, "tsconfig.json");
+
+module.exports = {
+  root: true,
+  extends: [
+    require.resolve("@vercel/style-guide/eslint/node"),
+    require.resolve("@vercel/style-guide/eslint/typescript"),
+    require.resolve("@vercel/style-guide/eslint/browser"),
+    require.resolve("@vercel/style-guide/eslint/react"),
+    require.resolve("@vercel/style-guide/eslint/next"),
+  ],
+  parserOptions: {
+    project,
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  rules: {
+    "import/no-default-export": "off",
+    "import/order": ["error", { "newlines-between": "always" }],
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};

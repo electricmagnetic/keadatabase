daadb1c1efca0f126b9c727508a5639243ac90ae
George Moon <george@moon.org.nz>
2023-12-14 19:20:27 +0100

    Changing map to use full Next

--- apps/map/next.config.js
+++ apps/map/next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  trailingSlash: true,
   swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;

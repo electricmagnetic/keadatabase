d7591a320b37246504236405b871e570d6c1b041
George Moon <george@carbonfarm.tech>
2024-02-26 19:52:53 +0100

    Package updates to new front-end, fixing turf version

--- packages/geospatial/helpers.ts
+++ packages/geospatial/helpers.ts
@@ -3,16 +3,16 @@ import { type Geometry } from "geojson";
 
 /**
  * Function to transform a regular array of data (objects) into a GeoJSON feature collection.
- * @param idKey key for obtaining the ID
- * @param geometryKey key for obtaining the geometry
- * @param data dataset
- * @returns FeatureCollection
+ * @param idKey -- key for obtaining the ID
+ * @param geometryKey -- key for obtaining the geometry
+ * @param data -- dataset
+ * @returns -- FeatureCollection
  */
 export const generateGeoJson = (idKey: string, geometryKey: string, data: Record<string, unknown>[]) => {
   return featureCollection(data.map(datum => {
     const { [idKey]: id, [geometryKey]: geometry } = datum;
 
-    if(!(typeof id == "string" || typeof id === "number")) throw new Error("Unexpected ID format");
+    if(!(typeof id === "string" || typeof id === "number")) throw new Error("Unexpected ID format");
 
     return feature(geometry as Geometry, datum, { id }); // TODO assertion
   }));

dff9b04afaeb2c480cc39737c29feb1bd61fb525
George Moon <george@carbonfarm.tech>
2024-03-11 01:02:06 +0100

    Added basic map to observations

--- apps/frontend-next/app/observations/[id]/page.tsx
+++ apps/frontend-next/app/observations/[id]/page.tsx
@@ -1,4 +1,6 @@
 import { type Metadata } from "next";
+import { GeoJSONLayer } from "geospatial/layers";
+import { generateGeoJson } from "geospatial/helpers";
 
 import { getObservation } from "../actions";
 
@@ -6,6 +8,8 @@ import Page from "@/app/_components/layout/Page";
 import { type PageWithIdProps } from "@/app/_components/api/schema";
 import Breadcrumbs from "@/app/_components/layout/Breadcrumbs";
 import Properties from "@/app/_components/layout/Properties";
+import BaseMap from "@/app/_components/geospatial/BaseMap";
+import Figure from "@/app/_components/layout/Figure";
 
 export async function generateMetadata({
   params: { id },
@@ -18,9 +22,9 @@ export async function generateMetadata({
 
 function OptionalTextBlock({ name, text }: { name: string; text: string }) {
   return text ? (
-    <Page.Section size="tiny">
+    <>
       <h2>{name}</h2> <p className="whitespace-pre-line">{text}</p>
-    </Page.Section>
+    </>
   ) : null;
 }
 
@@ -28,6 +32,10 @@ export default async function ObservationPage({
   params: { id },
 }: PageWithIdProps) {
   const observation = await getObservation(id);
+
+  const observationAsGeoJson = JSON.stringify(
+    generateGeoJson("id", "point_location", [observation]),
+  );
 
   return (
     <Page>
@@ -41,31 +49,53 @@ export default async function ObservationPage({
       </Page.Section>
       <Page.Section background="dull" size="medium">
         <h1>Observation {`#${observation.id}`}</h1>
-        <Properties>
-          <Properties.Item name="When">
+        <Properties className="row g-2">
+          <Properties.Item className="col-md-4" iconName="calendar" name="When">
             {observation.date_sighted} {observation.time_sighted}
           </Properties.Item>
-          <Properties.Item name="Where">
+          <Properties.Item className="col-md-4" iconName="geo-alt" name="Where">
             {observation.geocode}, {observation.region}
           </Properties.Item>
-          <Properties.Item name="Who">
-            {observation.contributor}
-          </Properties.Item>
-          <Properties.Item name="What">
+          <Properties.Item className="col-md-4" iconName="feather" name="What">
             {observation.get_sighting_type_display} {observation.number}{" "}
             {observation.number === 1 ? "bird" : "birds"}
           </Properties.Item>
-          <Properties.Item name="Status">
+          <Properties.Item
+            className="col-md-4"
+            iconName="patch-check-fill"
+            name="Status"
+          >
             {observation.get_status_display}
+          </Properties.Item>
+          <Properties.Item className="col-md-4" iconName="person" name="Who">
+            {observation.contributor}
           </Properties.Item>
         </Properties>
       </Page.Section>
-      <OptionalTextBlock name="Comments" text={observation.comments} />
-      <OptionalTextBlock
-        name="Location Details"
-        text={observation.location_details}
-      />
-      <OptionalTextBlock name="Behaviour" text={observation.behaviour} />
+      <Page.Section>
+        <div className="row">
+          <div className="col-md-6">
+            <OptionalTextBlock name="Comments" text={observation.comments} />
+            <OptionalTextBlock
+              name="Location Details"
+              text={observation.location_details}
+            />
+            <OptionalTextBlock name="Behaviour" text={observation.behaviour} />
+          </div>
+          <div className="col-md-6">
+            <Figure caption={`Map around ${observation.geocode}`}>
+              <div style={{ height: "720px" }}>
+                <BaseMap interactive={false}>
+                  <GeoJSONLayer
+                    geoJsonString={observationAsGeoJson}
+                    zoomToLayer
+                  />
+                </BaseMap>
+              </div>
+            </Figure>
+          </div>
+        </div>
+      </Page.Section>
     </Page>
   );
 }

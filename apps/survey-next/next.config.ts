dbdd9870ec2807d0105453edbc6c510ba7e2b0a2
George Moon <george@carbonfarm.tech>
2026-06-18 22:19:25 +0200

    survey-next: moving redirect again

--- apps/survey-next/next.config.ts
+++ apps/survey-next/next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
-const getApiBase = () => {
-  const apiBase = process.env.API_BASE;
-  if (!apiBase) throw new Error("Missing API_BASE environment variable");
-  return apiBase;
-};
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  rewrites: async () => [
-    {
-      source: "/api/:path*",
-      destination: `${getApiBase()}/:path*`,
-    },
-  ],
 };
 
 export default nextConfig;

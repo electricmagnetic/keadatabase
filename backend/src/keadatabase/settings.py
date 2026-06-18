cc5a201e70db383fe930e13309b5ab361a3e1cd1
George Moon <george@carbonfarm.tech>
2026-06-18 22:47:43 +0200

    backend: enable accounts views

--- backend/src/keadatabase/settings.py
+++ backend/src/keadatabase/settings.py
@@ -389,5 +389,5 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
 ACCOUNT_LOGIN_METHODS = {'email'}
 
-HEADLESS_ONLY = True
+HEADLESS_ONLY = False
 

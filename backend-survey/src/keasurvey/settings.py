9dc4fb0cb4f72744b154ec9fc81c7638327e8010
George Moon <george@carbonfarm.tech>
2026-06-30 00:11:46 +0200

    backend-survey: added allauth session token auth class

--- backend-survey/src/keasurvey/settings.py
+++ backend-survey/src/keasurvey/settings.py
@@ -218,6 +218,7 @@ REST_FRAMEWORK = {
     },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'allauth.headless.contrib.rest_framework.authentication.XSessionTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
